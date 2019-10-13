69914a7a111db19eab92a93746c7acd9d0b20467
James Lacandula <jlacandula@gmail.com>
2019-10-14 05:06:15 +0800

    end of C02_04

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 
 const app = express();
 
+app.use(bodyParser.json());
+
+//routes
 app.get('/hello', (req, res) => res.send('Hello CDO!'));
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
+
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 
 app.listen(8000, () => console.log('listening on PORT 8000!'));
