ca2c45ba27ef7b575f1945c69b0c71a1166b8d6e
James Lacandula <jlacandula@gmail.com>
2019-10-14 04:26:59 +0800

    server setup 1

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,3 +1,7 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/hello', (req, res) => res.send('Hello CDO!'));
+
+app.listen(8000, () => console.log('listening on PORT 8000!'));
