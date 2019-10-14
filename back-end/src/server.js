8726c0f1436a854a545e3ac039ca94ca61bbab13
James Lacandula <jlacandula@gmail.com>
2019-10-14 09:58:39 +0800

    ch2_6

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -4,12 +4,15 @@ import bodyParser from 'body-parser';
 const articlesInfo = {
     'learn-react': {
         upvotes: 0,
+        comments: [],
     },
     'learn-node': {
         upvotes: 0,
+        comments: [],
     },
     'my-thoughts-on-resumes': {
         upvotes: 0,
+        comments: [],
     },
 }
 
@@ -21,7 +24,13 @@ app.use(bodyParser.json());
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
     articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes! `);
+});
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+    articlesInfo[articleName].comments.push({ username, text });
+    res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(8000, () => console.log('listening on PORT 8000!'));
