56d405de702fa1bec8d134b5ab29d2a9f355ff73
James Lacandula <jlacandula@gmail.com>
2019-10-16 23:45:32 +0800

    end of ch3_3

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -6,15 +6,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api/articles/:name', async (red, res) => {
+app.get('/api/articles/:name', async (req, res) => {
     try {
         const articleName = req.params.name;
     
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('react-blog');
         
-        const articlesInfo = await db.collection('artitcles').findOne({ name: articleName });
-        res.status(200).json(articlesInfo);
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
     
         client.close();
     } catch (error) {
@@ -27,13 +27,13 @@ app.get('/api/articles/:name', async (red, res) => {
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
     articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes! `);
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
 });
 app.post('/api/articles/:name/add-comment', (req, res) => {
     const { username, text } = req.body;
     const articleName = req.params.name;
-    articlesInfo[articleName].comments.pgitush({ username, text });
+    articlesInfo[articleName].comments.push({ username, text });
     res.status(200).send(articlesInfo[articleName]);
 });
 
-app.listen(8000, () => console.log('listening on PORT 8000!'));
+app.listen(8000, () => console.log('Listening on port 8000!'));
