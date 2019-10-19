3da504de66ad2c8605e1188f4ff92a2f0fd017d0
James Lacandula <jlacandula@gmail.com>
2019-10-19 14:56:21 +0800

    setup app for release

---
+++ front-end/build/precache-manifest.e4d12c9bf947c4f926e73ee74a79e0af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9dcc30e44d63cad83cd4",
+    "url": "/static/js/main.307d99a1.chunk.js"
+  },
+  {
+    "revision": "cd72b8b2591135dcf146",
+    "url": "/static/js/2.7b7ac67d.chunk.js"
+  },
+  {
+    "revision": "9dcc30e44d63cad83cd4",
+    "url": "/static/css/main.3d31e828.chunk.css"
+  },
+  {
+    "revision": "15c9c46fcfc9ff9f38b88f20d3b336a6",
+    "url": "/index.html"
+  }
+];
