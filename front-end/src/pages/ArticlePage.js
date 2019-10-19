95498b940c391687a96477b2d73a70c27025f405
James Lacandula <jlacandula@gmail.com>
2019-10-19 13:31:51 +0800

    add useState and useEffect

--- front-end/src/pages/ArticlePage.js
+++ front-end/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
@@ -8,6 +8,15 @@ const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes:0,
+        comments: []
+    });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) });
+    }, [name]);
+
     if (!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name );
@@ -15,6 +24,7 @@ const ArticlePage = ({ match }) => {
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This post has been upvoted {articleInfo.upvotes} times </p>
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
         ))}
