a170369a2aafa3e1a851e8fab9aeb4c88d968440
James Lacandula <jlacandula@gmail.com>
2019-10-19 13:14:03 +0800

    add whatwg-fetch for IE compatibility

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -60,6 +60,7 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName});
         res.status(200).json(updatedArticleInfo);
     }, res);
+    
 });
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
--- front-end/src/pages/ArticlePage.js
+++ front-end/src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
+
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
